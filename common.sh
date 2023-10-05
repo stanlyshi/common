63221badef25b8d86dfcf8e3d9c73ca73ec280a5
stanlyshi <85752240+stanlyshi@users.noreply.github.com>
2023-10-05 13:47:57 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -11,49 +11,17 @@ MAGENTA_COLOR="\e[35m"
 CYAN_COLOR="\e[36m"
 WHITE_COLOR="\e[37m"
 
-function __error_msg() {
-	echo -e "${RED_COLOR}[ERROR]${RESET_COLOR} $*"
-}
-
-function __success_msg() {
-	echo -e "${GREEN_COLOR}[SUCCESS]${RESET_COLOR} $*"
-}
-
-function __warning_msg() {
-	echo -e "${YELLOW_COLOR}[WARNING]${RESET_COLOR} $*"
-}
-
-function __info_msg() {
-	echo -e "${CYAN_COLOR}[INFO]${RESET_COLOR} $*"
-}
-
-function __red_color() {
-	echo -e "${RED_COLOR}$*${RESET_COLOR}"
-}
-
-function __green_color() {
-	echo -e "${GREEN_COLOR}$*${RESET_COLOR}"
-}
-
-function __yellow_color() {
-	echo -e "${YELLOW_COLOR}$*${RESET_COLOR}"
-}
-
-function __blue_color() {
-	echo -e "${BLUE_COLOR}$*${RESET_COLOR}"
-}
-
-function __magenta_color() {
-	echo -e "${MAGENTA_COLOR}$*${RESET_COLOR}"
-}
-
-function __cyan_color() {
-	echo -e "${CYAN_COLOR}$*${RESET_COLOR}"
-}
-
-function __white_color() {
-	echo -e "${WHITE_COLOR}$*${RESET_COLOR}"
-}
+function __error_msg() { echo -e "${RED_COLOR}[ERROR]${RESET_COLOR} $*" }
+function __success_msg() { echo -e "${GREEN_COLOR}[SUCCESS]${RESET_COLOR} $*" }
+function __warning_msg() { echo -e "${YELLOW_COLOR}[WARNING]${RESET_COLOR} $*" }
+function __info_msg() { echo -e "${CYAN_COLOR}[INFO]${RESET_COLOR} $*" }
+function __red_color() { echo -e "${RED_COLOR}$*${RESET_COLOR}" }
+function __green_color() { echo -e "${GREEN_COLOR}$*${RESET_COLOR}" }
+function __yellow_color() { echo -e "${YELLOW_COLOR}$*${RESET_COLOR}" }
+function __blue_color() {  echo -e "${BLUE_COLOR}$*${RESET_COLOR}" }
+function __magenta_color() { echo -e "${MAGENTA_COLOR}$*${RESET_COLOR}" }
+function __cyan_color() { echo -e "${CYAN_COLOR}$*${RESET_COLOR}" }
+function __white_color() { echo -e "${WHITE_COLOR}$*${RESET_COLOR}" }
 
 ################################################################################################################
 # 环境变量
@@ -94,7 +62,6 @@ function parse_settings() {
 		NOTICE_TYPE="false"
 	fi
 
-	
 	if [[ ${PACKAGES_ADDR} == "default" ]]; then
 		PACKAGES_ADDR="roacn/openwrt-packages"
 	fi
@@ -185,7 +152,6 @@ function parse_settings() {
 	echo FILENAME_DEFAULT_RUNONCE="default_settings_runonce" >> ${GITHUB_ENV}
 	echo FILENAME_CONFIG_GEN="config_generate" >> ${GITHUB_ENV}
 	echo FILENAME_TO_DELETE="default_delete" >> ${GITHUB_ENV}
-
 }
 
 ################################################################################################################
