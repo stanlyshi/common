eda73859727fdda2df3111c07eec68b9c2d886a5
roacn <85752240+stanlyshi@users.noreply.github.com>
2023-10-20 15:12:41 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -1125,7 +1125,8 @@ function make_defconfig() {
 ################################################################################################################
 function compile_info() {	
 	echo
-	__red_color "OpenWrt固件信息"
+	__red_color "固件信息"
+	echo "--------------------------------------------------------------------------------"
 	__blue_color "编译源码: ${SOURCE}"
 	__blue_color "源码链接: ${SOURCE_URL}"
 	__blue_color "源码分支: ${SOURCE_BRANCH}"
@@ -1142,10 +1143,13 @@ function compile_info() {
 
 	echo
 	__red_color "固件类型"
+	echo "--------------------------------------------------------------------------------"
 	if [[ "${FIRMWARE_TYPE}" == "lxc" ]]; then
 		__blue_color "LXC固件：开启"
 		echo
-		__red_color "LXC固件自动更新"
+		echo
+		__red_color "固件更新"
+		echo "--------------------------------------------------------------------------------"
 		__white_color "1、PVE运行："
 		__green_color "wget https://ghproxy.com/https://raw.githubusercontent.com/stanlyshi/pve/main/openwrt.lxc.sh -O /usr/bin/openwrt && chmod +x /usr/bin/openwrt"
 		__white_color "2、PVE运行："
@@ -1154,7 +1158,9 @@ function compile_info() {
 	else
 		__white_color "LXC固件：关闭"
 		echo
-		__red_color "自动更新信息"
+		echo
+		__red_color "固件更新"
+		echo "--------------------------------------------------------------------------------"
 		__blue_color "插件版本: ${AUTOUPDATE_VERSION}"
 		
 		if [[ "${TARGET_BOARD}" == "x86" ]]; then
@@ -1171,7 +1177,8 @@ function compile_info() {
 	fi
 	
 	echo
-	__red_color "Github在线编译配置"
+	__red_color "编译选项"
+	echo "--------------------------------------------------------------------------------"
 	if [[ "${UPLOAD_RELEASE}" == "true" ]]; then
 		__blue_color "发布firmware+ipk至Github Relese: 开启"
 	else
@@ -1230,6 +1237,7 @@ function compile_info() {
 	echo "${plugin_2}" >plugins_info
 	if [ -n "$(ls -A "${HOME_PATH}/plugins_info" 2>/dev/null)" ]; then
 		__red_color "插件列表"
+		echo "--------------------------------------------------------------------------------"
 		chmod -Rf +x ${HOME_PATH}/plugins_info
 		source ${HOME_PATH}/plugins_info
 		rm -rf ${HOME_PATH}/plugins_info
@@ -1237,7 +1245,8 @@ function compile_info() {
 	fi
 	
 	if [[ -s ${CONFFLICTIONS} ]]; then
-		__red_color "插件冲突信息"
+		__red_color "冲突信息"
+		echo "--------------------------------------------------------------------------------"
 		chmod +x ${CONFFLICTIONS} && source ${CONFFLICTIONS}
 		rm -rf ${CONFFLICTIONS}
 	fi
